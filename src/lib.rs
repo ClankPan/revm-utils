d85a8c6863085941eae54cb84c7fbe5ff11a44c4
clankpan <clankpan26cm@gmail.com>
2025-03-02 17:11:23 +0900

    commentout

--- src/lib.rs
+++ src/lib.rs
@@ -1,101 +1,101 @@
 // use revm::{primitives::TxKind, Context, ExecuteCommitEvm, MainBuilder, MainContext};
 // use revm_database::{CacheDB, StateBuilder};
-use serde::{Deserialize, Serialize};
-use std::convert::Infallible;
+// use serde::{Deserialize, Serialize};
+// use std::convert::Infallible;
 
-use revm::{
-    context::AccessList,
-    primitives::{Address, Bytes, B256, U256},
-    state::{AccountInfo, Bytecode},
-    Database, DatabaseRef,
-};
+// use revm::{
+//     context::AccessList,
+//     primitives::{Address, Bytes, B256, U256},
+//     state::{AccountInfo, Bytecode},
+//     Database, DatabaseRef,
+// };
 
-#[derive(Serialize, Deserialize)]
-pub struct DummyDB {}
+// #[derive(Serialize, Deserialize)]
+// pub struct DummyDB {}
 
 pub mod eth_transfer;
 pub use eth_transfer::*;
 
-impl DummyDB {
-    pub fn new() -> Self {
-        Self {}
-    }
-}
+// impl DummyDB {
+//     pub fn new() -> Self {
+//         Self {}
+//     }
+// }
 
-impl Database for DummyDB {
-    #[doc = " The database error type."]
-    type Error = Infallible;
+// impl Database for DummyDB {
+//     #[doc = " The database error type."]
+//     type Error = Infallible;
 
-    #[doc = " Gets basic account information."]
-    fn basic(&mut self, _address: Address) -> Result<Option<AccountInfo>, Self::Error> {
-        todo!()
-    }
+//     #[doc = " Gets basic account information."]
+//     fn basic(&mut self, _address: Address) -> Result<Option<AccountInfo>, Self::Error> {
+//         todo!()
+//     }
 
-    #[doc = " Gets account code by its hash."]
-    fn code_by_hash(&mut self, _code_hash: B256) -> Result<Bytecode, Self::Error> {
-        todo!()
-    }
+//     #[doc = " Gets account code by its hash."]
+//     fn code_by_hash(&mut self, _code_hash: B256) -> Result<Bytecode, Self::Error> {
+//         todo!()
+//     }
 
-    #[doc = " Gets storage value of address at index."]
-    fn storage(&mut self, _address: Address, _index: U256) -> Result<U256, Self::Error> {
-        todo!()
-    }
+//     #[doc = " Gets storage value of address at index."]
+//     fn storage(&mut self, _address: Address, _index: U256) -> Result<U256, Self::Error> {
+//         todo!()
+//     }
 
-    #[doc = " Gets block hash by block number."]
-    fn block_hash(&mut self, _number: u64) -> Result<B256, Self::Error> {
-        todo!()
-    }
-}
+//     #[doc = " Gets block hash by block number."]
+//     fn block_hash(&mut self, _number: u64) -> Result<B256, Self::Error> {
+//         todo!()
+//     }
+// }
 
-impl DatabaseRef for DummyDB {
-    #[doc = " The database error type."]
-    type Error = Infallible;
+// impl DatabaseRef for DummyDB {
+//     #[doc = " The database error type."]
+//     type Error = Infallible;
 
-    #[doc = " Gets basic account information."]
-    fn basic_ref(&self, _address: Address) -> Result<Option<AccountInfo>, Self::Error> {
-        todo!()
-    }
+//     #[doc = " Gets basic account information."]
+//     fn basic_ref(&self, _address: Address) -> Result<Option<AccountInfo>, Self::Error> {
+//         todo!()
+//     }
 
-    #[doc = " Gets account code by its hash."]
-    fn code_by_hash_ref(&self, _code_hash: B256) -> Result<Bytecode, Self::Error> {
-        todo!()
-    }
+//     #[doc = " Gets account code by its hash."]
+//     fn code_by_hash_ref(&self, _code_hash: B256) -> Result<Bytecode, Self::Error> {
+//         todo!()
+//     }
 
-    #[doc = " Gets storage value of address at index."]
-    fn storage_ref(&self, _address: Address, _index: U256) -> Result<U256, Self::Error> {
-        todo!()
-    }
+//     #[doc = " Gets storage value of address at index."]
+//     fn storage_ref(&self, _address: Address, _index: U256) -> Result<U256, Self::Error> {
+//         todo!()
+//     }
 
-    #[doc = " Gets block hash by block number."]
-    fn block_hash_ref(&self, _number: u64) -> Result<B256, Self::Error> {
-        todo!()
-    }
-}
+//     #[doc = " Gets block hash by block number."]
+//     fn block_hash_ref(&self, _number: u64) -> Result<B256, Self::Error> {
+//         todo!()
+//     }
+// }
 
-#[derive(Serialize, Deserialize)]
-pub struct BlockInfo {
-    pub number: u64,
-    pub beneficiary: Address,
-    pub timestamp: u64,
-    pub difficulty: U256,
-    pub gas_limit: u64,
-    pub basefee: u64,
-    pub transactions: Vec<TransactionInfo>,
-}
+// #[derive(Serialize, Deserialize)]
+// pub struct BlockInfo {
+//     pub number: u64,
+//     pub beneficiary: Address,
+//     pub timestamp: u64,
+//     pub difficulty: U256,
+//     pub gas_limit: u64,
+//     pub basefee: u64,
+//     pub transactions: Vec<TransactionInfo>,
+// }
 
-#[derive(Serialize, Deserialize, Clone)]
-pub struct TransactionInfo {
-    pub from: Address,
-    pub gas_limit: u64,
-    pub gas_price: u128,
-    pub value: U256,
-    pub input: Bytes,
-    pub max_priority_fee_per_gas: Option<u128>,
-    pub chain_id: Option<u64>,
-    pub nonce: u64,
-    pub access_list: Option<AccessList>,
-    pub to: Option<Address>,
-}
+// #[derive(Serialize, Deserialize, Clone)]
+// pub struct TransactionInfo {
+//     pub from: Address,
+//     pub gas_limit: u64,
+//     pub gas_price: u128,
+//     pub value: U256,
+//     pub input: Bytes,
+//     pub max_priority_fee_per_gas: Option<u128>,
+//     pub chain_id: Option<u64>,
+//     pub nonce: u64,
+//     pub access_list: Option<AccessList>,
+//     pub to: Option<Address>,
+// }
 
 // const BYTES: &[u8] = include_bytes!("../../helper/block_state_caches/block_10889449.bin");
 
